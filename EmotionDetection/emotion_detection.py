0aede26c63a2d7205787432837e5d269387a7ee4
bob-conroy <bob.conroymba@gmail.com>
2024-11-18 16:52:55 -0500

    Updates made

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,17 +6,25 @@ def emotion_detector(text_to_analyze):
     header =  {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header)
+        
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response['emotionPredictions'][0]
+        
+        anger_score = emotions['emotion']['anger']
+        disgust_score = emotions['emotion']['disgust']
+        fear_score = emotions['emotion']['fear']
+        joy_score = emotions['emotion']['joy']
+        sadness_score = emotions['emotion']['sadness']
+        dominant_emotion = max(emotions['emotion'], key = emotions['emotion'].get)
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion = None
 
-    formatted_response = json.loads(response.text)
-    emotions = formatted_response['emotionPredictions'][0]
-    
-    anger_score = emotions['emotion']['anger']
-    disgust_score = emotions['emotion']['disgust']
-    fear_score = emotions['emotion']['fear']
-    joy_score = emotions['emotion']['joy']
-    sadness_score = emotions['emotion']['sadness']
-    dominant_emotion = max(emotions['emotion'], key = emotions['emotion'].get)
-    
     return {'anger': anger_score, 
             'disgust': disgust_score,
             'fear': fear_score,
--- server.py
+++ server.py
@@ -1,41 +1,41 @@
 ''' Executing this function initiates the application of emotion detection
-    to be executed over the Flask channel and deployed on
-    localhost:5000.
+    to be executed over the Flask channel and deployed on localhost:5000.
 '''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 #Initiate the flask app : TODO
-app = Flask("Emotion Detector)
+app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
-    ''' This code receives the text from the HTML interface and 
-        runs emotion detection over it using emotion_detector()
-        function. The output returned shows the dominant emotion
+    ''' This code receives the text from the HTML interface and runs emotion detection over it
+        using emotion_detector() function. The output returned shows the dominant emotion
     '''
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyze)
-
+    
     # Extract the label and score from the response
-    anger_score = response['anger_score']
-    disgust_score = response['disgust_score']
-    fear_score = response['fear_score']
-    joy_score = response['joy_score']
-    sadness_score = response['sadness_score']
+    anger_score = response['anger']
+    disgust_score = response['disgust']
+    fear_score = response['fear']
+    joy_score = response['joy']
+    sadness_score = response['sadness']
     dominant_emotion = response['dominant_emotion']
         
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!."
+
     # Return a formatted string with the sentiment label and score
     return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(
         anger_score, disgust_score, fear_score, joy_score, sadness_score, dominant_emotion)
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
+    '''  This function initiates the rendering of the main application page over the Flask channel
     '''
     return render_template('index.html')
 
